2e9ad9c3dc349eb7a3596460be44f7315eacfc0f
Ove Gram Nipen <ove@nipen.no>
2014-03-18 20:38:55 +0100

    Fjernet duplisering ved å loope over dagene
    - Laget egne klasser for Prosjekt og Dag
    - Innførte array av ukedager som man kan loope over

--- controller.js
+++ controller.js
@@ -1,12 +1,30 @@
+var ukedager = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
+
+function Dag(ukedag) {
+  this.ukedag = ukedag;
+  this.timer = 0;
+  this.beskrivelse = "";
+}
+
+function Prosjekt(navn) {
+  this.navn = navn;
+  this.dager = [];
+  for (var i = 0; i < 7; i++) {
+    this.dager.push(new Dag(i));
+  }
+}
+
 function ProsjektController($scope) {
-  $scope.prosjekter = [
-  {navn:'Pingvinen'},
-  {navn:'Lunch - ikke fakturerbar'},
-  {navn:'Aladdin Oil - Nytt intranett'}
+  $scope.prosjekter = [ 
+  new Prosjekt("Pingvinen"), 
+  new Prosjekt("Lunch - ikke fakturerbar"), 
+  new Prosjekt("Aladdin Oil - Nytt intranett") 
   ];
 
+  $scope.ukedager = ukedager;
+
   $scope.addProsjekt = function() {
-    $scope.prosjekter.push({navn: $scope.prosjektnavn});
+    $scope.prosjekter.push(new Prosjekt($scope.prosjektnavn));
     $scope.prosjektnavn = '';
   };
 
